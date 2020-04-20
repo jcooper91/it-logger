7235b885b2b80f591a6c3b7223a03388e3e0f065
JCooper91 <jackcooper246@gmail.com>
2020-04-20 18:15:01 +0100

    Added redux store and reducer for logs

--- src/App.js
+++ src/App.js
@@ -8,6 +8,8 @@ import AddTechModal from './components/techs/AddTechModal'
 import TechListModal from './components/techs/TechListModal'
 import 'materialize-css/dist/css/materialize.min.css'
 import M from 'materialize-css/dist/js/materialize.min.js'
+import { Provider } from 'react-redux'
+import store from './store'
 
 const App = () => {
 
@@ -17,17 +19,19 @@ const App = () => {
   })
 
   return (
-    <Fragment>
-      <SearchBar />
-      <div className="container">
-        <AddBtn />
-        <AddLogModal />
-        <EditLogModal />
-        <AddTechModal />
-        <TechListModal />
-        <Logs />
-      </div>
-    </Fragment>
+    <Provider store={store}>
+      <Fragment>
+        <SearchBar />
+        <div className="container">
+          <AddBtn />
+          <AddLogModal />
+          <EditLogModal />
+          <AddTechModal />
+          <TechListModal />
+          <Logs />
+        </div>
+      </Fragment>
+    </Provider>
   )
 }
 
